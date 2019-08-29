2024e65db82093c717921bd3912d32acc8986ff5
Illia Pazynych <irbis691@gmail.com>
2019-08-30 00:49:21 +0300

    Improve basic algorithm, make it work with all provided diff case files. Make target element id configurable through command line arguments. Switch from standard output to output to file in resource folder.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Current implementation just search by class attribute and then finds element with correct text.
+<p>Implementation works for all provided diff case files.</p>
+<p>Also it is possible to set different target element id as a third command line argument.</p>
+<p>
+Basically, it is just trying to find <b>visible</b> element with same text, if not found then search by class. 
+If still not found / there are more then one result then try to search by title / try to search by class and title in addition.
+</p>
--- src/main/java/com/test_task/App.java
+++ src/main/java/com/test_task/App.java
@@ -1,6 +1,7 @@
 package com.test_task;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -9,6 +10,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -17,36 +22,71 @@ public class App {
 	private static Logger LOGGER = LoggerFactory.getLogger(App.class);
 
 	private static String CHARSET_NAME = "utf8";
-	private static String TARGET_ELEMENT_ID = "make-everything-ok-button";
 
 	public static void main(String[] args) {
 		String originFilePath = args[0];
 		String diffCaseFilePath = args[1];
+		String targetElementId = args[2];
 
 		File originFile = new File(originFilePath);
 		File diffCaseFile = new File(diffCaseFilePath);
+		Path outputPath = Paths.get("src/main/resources/comparisonOutputForSamplePages.txt");
 
-		Optional<Element> buttonOrigin = findElementById(originFile, TARGET_ELEMENT_ID);
-		String buttonText = buttonOrigin.map(b -> b.childNode(0).attributes().asList().get(0).getValue()).get();
-		Map<String, String> buttonAttr = new HashMap<>();
-		buttonOrigin.ifPresent(element -> element.attributes().asList()
-				.forEach(attr -> buttonAttr.put(attr.getKey(), attr.getValue())));
-
-		StringBuilder cssQuery = new StringBuilder("a[class*=\"" + buttonAttr.get("class") + "\"]");
-		Optional<Elements> optElements = findElementsByQuery(diffCaseFile, cssQuery.toString());
-		if (optElements.isPresent()) {
-			Elements elements = optElements.get();
-			if (elements.size() > 1) {
-				cssQuery.insert(cssQuery.length(), "[title*=\"" + buttonAttr.get("title") + "\"]");
-				optElements = findElementsByQuery(diffCaseFile, cssQuery.toString());
-				optElements.ifPresent(value -> System.out.println(buildPathToDiffCaseElement(value.get(0))));
-			} else {
-				System.out.println(buildPathToDiffCaseElement(elements.get(0)));
-			}
+		Optional<Element> originalButton = findElementById(originFile, targetElementId);
+		if (originalButton.isPresent()) {
+			String buttonText = originalButton.map(b -> b.childNode(0).attributes().asList().get(0).getValue()).get().trim();
+			Optional<Elements> optElements = findElementsByQuery(diffCaseFile, "a:contains(" + buttonText + ")");
+			optElements.ifPresent(elements -> {
+				try {
+					Files.write(outputPath,
+							(getPathToDiffCaseElement(diffCaseFile, originalButton.get(), elements) + "\n").getBytes(),
+							StandardOpenOption.APPEND);
+				} catch (IOException e) {
+					LOGGER.error("Error writing result to file", e);
+				}
+			});
 		}
 	}
 
-	private static String buildPathToDiffCaseElement(Element element) {
+	private static String getPathToDiffCaseElement(File diffCaseFile, Element originalButton, Elements elements) {
+		if (elements.size() != 1) {
+			List<Element> visibleElements = elements.stream()
+					.filter(App::isElementVisible)
+					.collect(Collectors.toList());
+			if (visibleElements.size() != 1) {
+				Map<String, String> buttonAttr = originalButton.attributes().asList().stream()
+						.collect(Collectors.toMap(Attribute::getKey, Attribute::getValue));
+				return findDiffCaseElementByCssQuery(diffCaseFile, buttonAttr, "class");
+			}
+		}
+		return getPathToDiffCaseElement(elements.get(0));
+	}
+
+	private static boolean isElementVisible(Element element) {
+		return !(element.attr("style").equals("display:none"));
+	}
+
+	private static String findDiffCaseElementByCssQuery(File diffCaseFile, Map<String, String> buttonAttr, String attrName) {
+		StringBuilder cssQuery = new StringBuilder("a[" + attrName + "*=\"" + buttonAttr.get(attrName) + "\"]");
+		Optional<Elements> optElements = findElementsByQuery(diffCaseFile, cssQuery.toString());
+		if (optElements.isPresent()) {
+			Elements elements = optElements.get();
+			if (elements.size() == 0) {
+				return findDiffCaseElementByCssQuery(diffCaseFile, buttonAttr, "title");
+			} else if (elements.size() > 1) {
+				cssQuery.insert(cssQuery.length(), "[title*=\"" + buttonAttr.get("title") + "\"]");
+				optElements = findElementsByQuery(diffCaseFile, cssQuery.toString());
+				if (optElements.isPresent()) {
+					return getPathToDiffCaseElement(optElements.get().get(0));
+				}
+			} else {
+				return getPathToDiffCaseElement(elements.get(0));
+			}
+		}
+		return "Diff case not found";
+	}
+
+	private static String getPathToDiffCaseElement(Element element) {
 		StringBuilder res = new StringBuilder();
 		while (element.parent() != null) {
 			res.insert(0, " > " + element.tag());
